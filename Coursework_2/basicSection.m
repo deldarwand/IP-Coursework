b452206531f7dc7ece1d46f19b220d07df7181f3
Daniel <daniel.eldar@gmail.com>
2016-12-02 14:32:50 +0000

    Added the naive calculation of the squared sum difference.

--- Coursework_2/basicSection.m
+++ Coursework_2/basicSection.m
@@ -2,20 +2,20 @@
 
 % Row and column of the pixel for which we wish to find all similar patches 
 % NOTE: For this section, we pick only one patch
-row = 10;
-col = 10;
+row = 100;
+col = 100;
 
 % Patchsize - make sure your code works for different values
-patchSize = 0;
+patchSize = 5;
 
 % Search window size - make sure your code works for different values
-searchWindowSize = 0;
+searchWindowSize = 5;
 
 
 %% Implementation of work required in your basic section-------------------
 
 % TODO - Load Image
-image = zeros(100, 100);
+image = imread('images/alleyNoisy_sigma20.png');%zeros(100, 100);
 
 % TODO - Fill out this function
 image_ii = computeIntegralImage(image);
@@ -27,18 +27,20 @@ figure('name', 'Normalised Integral Image');
 
 % TODO - Template matching for naive SSD (i.e. just loop and sum)
 [offsetsRows_naive, offsetsCols_naive, distances_naive] = templateMatchingNaive(row, col,...
-    patchSize, searchWindowSize);
+    patchSize, searchWindowSize, image);
 
 % TODO - Template matching using integral images
 [offsetsRows_ii, offsetsCols_ii, distances_ii] = templateMatchingIntegralImage(row, col,...
-    patchSize, searchWindowSize);
+    patchSize, searchWindowSize, image);
 
 %% Let's print out your results--------------------------------------------
 
 % NOTE: Your results for the naive and the integral image method should be
 % the same!
-for i=1:length(offsetsRows_naive)
-    disp(['offset rows: ', num2str(offsetsRows_naive(i)), '; offset cols: ',...
-        num2str(offsetsCols_naive(i)), '; Naive Distance = ', num2str(distances_naive(i),10),...
-        '; Integral Im Distance = ', num2str(distances_ii(i),10)]);
+length(distances_naive)
+for i=1:length(distances_naive)
+    disp([...%'offset rows: ', num2str(offsetsRows_naive(i)), '; offset cols: ',...
+        %num2str(offsetsCols_naive(i)), ...
+        '; Naive Distance = ', num2str(distances_naive(i),10)]);%,...
+       % '; Integral Im Distance = ', num2str(distances_ii(i),10)]);
 end
--- Coursework_2/templateMatchingNaive.m
+++ Coursework_2/templateMatchingNaive.m
@@ -1,5 +1,5 @@
 function [offsetsRows, offsetsCols, distances] = templateMatchingNaive(row, col,...
-    patchSize, searchWindowSize)
+    patchSize, searchWindowSize, image)
 % This function should for each possible offset in the search window
 % centred at the current row and col, save a value for the offsets and
 % patch distances, e.g. for the offset (-1,-1)
@@ -12,9 +12,76 @@ function [offsetsRows, offsetsCols, distances] = templateMatchingNaive(row, col,
 % current offset
 
 %REPLACE THIS
+isSearchWindowSizeEven = mod(searchWindowSize, 2);
 
-offsetsRows = zeros(10,1);
-offsetsCols = zeros(10,1);
-distances = randn(10, 1);
+lowerYValue = -1; lowerXValue = -1; upperYValue = -1; upperXValue = -1;
+
+if(isSearchWindowSizeEven == 0) %The search size is even.
+    lowerOffset = (searchWindowSize - 2) / 2;
+    upperOffset = searchWindowSize / 2;
+    
+    lowerYValue = row - lowerOffset;
+    lowerXValue = col - lowerOffset;
+    upperYValue = row + upperOffset;
+    upperXValue = col + upperOffset;
+    
+else %The search size is odd.
+    offset = (searchWindowSize - 1) / 2;
+    
+    lowerYValue = row - offset;
+    lowerXValue = col - offset;
+    upperYValue = row + offset;
+    upperXValue = col + offset;
+    
+end
+
+offsetsRows = zeros(searchWindowSize,1);
+offsetsCols = zeros(searchWindowSize,1);
+distances = zeros(searchWindowSize*searchWindowSize, 1);
+
+centrePatchSum = getSumOfPatch(image, row, col, patchSize);
+counter = 1;
+for x = lowerXValue : upperXValue
+    for y = lowerYValue : upperYValue
+        patchDistance = getSumOfPatch(image, x, y, patchSize) - centrePatchSum;
+        distances(counter) = patchDistance * patchDistance;
+        counter = counter+1;
+    end
+end
+end
+
+function sumOfPatch = getSumOfPatch(image, xPosition, yPosition, patchSize)
+
+isPatchSizeEven = mod(patchSize, 2);
+
+lowerYValue = -1; lowerXValue = -1; upperYValue = -1; upperXValue = -1;
+
+if(isPatchSizeEven == 0) %The patch size is even.
+    lowerOffset = (patchSize - 2) / 2;
+    upperOffset = patchSize / 2;
+    
+    lowerYValue = yPosition - lowerOffset;
+    lowerXValue = xPosition - lowerOffset;
+    upperYValue = yPosition + upperOffset;
+    upperXValue = xPosition + upperOffset;
+    
+else %The search size is odd.
+    offset = (patchSize - 1) / 2;
+    
+    lowerYValue = yPosition - offset;
+    lowerXValue = xPosition - offset;
+    upperYValue = yPosition + offset;
+    upperXValue = xPosition + offset;
+    
+end
+
+sumOfPatch = double(0);
+
+for x = lowerXValue : upperXValue
+    for y = lowerYValue : upperYValue
+        sumOfPatch = double(sumOfPatch) + double(image(x, y));
+    end
+end
+
 
 end
